0bc84d469fafe8bc8a63d7ce2e75029d03008785
JekIquin <jci.dev.0@gmail.com>
2021-11-22 21:02:53 -0800

    added placeholders for typefaces

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-	return <div className="App">Jerick</div>;
+	return (
+		<div className="App">
+			<h1>Jerick</h1>
+		</div>
+	);
 }
 
 export default App;
